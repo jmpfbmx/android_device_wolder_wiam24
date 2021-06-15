00552099e64bb414ac3a0f5b5ff44735f3947992
jmpfbmx <jmpf.bmx@gmail.com>
2021-06-15 10:53:11 +0200

    wiam24: Set correct path
     * This is Wolder not DOOGEE XD
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I338d13ab54f687b5e5a06ff0c675720fa2373f03

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 # ProjectConfig
-include device/DOOGEE/$(MTK_TARGET_PROJECT)/ProjectConfig.mk
+include device/wolder/$(MTK_TARGET_PROJECT)/ProjectConfig.mk
 
 # Common mt6580 device
 $(call inherit-product, device/mediatek/mt6580/device.mk)
